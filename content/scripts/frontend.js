fe62b81630b2d95c818e0e09191e852fbc6daa95
matthewsotoudeh <matthewsot@outlook.com>
2017-02-03 20:24:56 -0800

    fixed a frontend bug

--- content/scripts/frontend.js
+++ content/scripts/frontend.js
@@ -1,15 +1,15 @@
 $("#calibrate").click(function () {
     if ($(this).text().trim() == "Start Calibration") {
-        window.sendServerMessage({ type: "command", command: "start-calibration" });
+        window.sendServerMessage({ forward: true, type: "command", command: "start-calibration" });
     } else {
-        window.sendServerMessage({ type: "command", command: "stop-calibration" });
+        window.sendServerMessage({ forward: true, type: "command", command: "stop-calibration" });
     }
 });
 
 $("#predict-speaker").click(function () {
     if ($("#predict-speaker").text().trim() == "Start Speaker Recognition Demo") {
-        window.sendServerMessage({ type: "command", command: "start-speaker-rec" });
+        window.sendServerMessage({ forward: true, type: "command", command: "start-speaker-rec" });
     } else {
-        window.sendServerMessage({ type: "command", command: "stop-speaker-rec" });
+        window.sendServerMessage({ forward: true, type: "command", command: "stop-speaker-rec" });
     }
 });
