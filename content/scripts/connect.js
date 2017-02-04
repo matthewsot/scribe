4fc46aaf6f4a97cd1f2bc2375e228f05c3c65a4c
matthewsotoudeh <matthewsot@outlook.com>
2017-02-03 21:22:14 -0800

    meeting creation flow updated

--- content/scripts/connect.js
+++ content/scripts/connect.js
@@ -1,26 +1,14 @@
-var peer = new Peer({key: 'yhaxhyw8324ndn29'});
+window.peer = new Peer({key: 'yhaxhyw8324ndn29'});
 
 window.attendees = [ ];
 window.clients = [ ];
 
 peer.on('open', function (id) {
-    window.attendees.push({ peerId: peer.id, name: "Host" });
+    window.switchWedgeContent("#landing-overlay");
+    return;
     var createNew = confirm("Create new room?");
     window.isHost = createNew;
     if (createNew) {
-        document.getElementById("status").textContent = "Room ID: " + id;
-        peer.on('connection', function (client) {
-            client.on('data', function (data) {
-                if (typeof data == "string") {
-                    attendees.push({ name: data, peerId: client.peer });
-                    window.sendAllClients(attendees);
-                } else {
-                    window.handleClientMessage(data, client);
-                }
-            });
-            console.log("Connected!");
-            window.clients.push(client);
-        });
     } else {
         var peerId = prompt("Enter room ID");
         window.name = prompt("What's your name?");
--- content/scripts/frontend.js
+++ content/scripts/frontend.js
@@ -1,3 +1,12 @@
+$("body").on("keypress", "input", function (e) {
+    if (e.which == 13) {
+        var btn = $(this).siblings("button");
+        if (btn.length) {
+            btn.click();
+        }
+    }
+});
+
 $("#calibrate").click(function () {
     if ($(this).text().trim() == "Start Calibration") {
         window.sendServerMessage({ forward: true, type: "command", command: "start-calibration" });
@@ -12,4 +21,34 @@ $("#predict-speaker").click(function () {
     } else {
         window.sendServerMessage({ forward: true, type: "command", command: "stop-speaker-rec" });
     }
+});
+
+wedge.show("#loading-overlay", { autoPositionType: 2, allowExit: false });
+
+window.switchWedgeContent = function (newContent) {
+    $("#wedge-content").children().hide().appendTo("body");
+    $(newContent).show();
+    $("#wedge-content").append($(newContent));
+};
+
+$("#create-meeting").click(function () {
+    window.switchWedgeContent("#new-mtg-overlay");
+});
+$("#do-create-meeting").click(function () {
+    window.attendees.push({ peerId: peer.id, name: $("#host-name").val().trim() });
+    window.isHost = true;
+    $("#mtg-id").text(peer.id);
+    peer.on('connection', function (client) {
+        client.on('data', function (data) {
+            if (typeof data == "string") {
+                attendees.push({ name: data, peerId: client.peer });
+                window.sendAllClients(attendees);
+            } else {
+                window.handleClientMessage(data, client);
+            }
+        });
+        console.log("Connected!");
+        window.clients.push(client);
+    });
+    window.switchWedgeContent("#waiting-host-overlay");
 });
