61a9ba4d1c683f304c7c8dea811586c56fcf98ed
matthewsotoudeh <matthewsot@outlook.com>
2017-02-03 23:29:45 -0800

    added browser compat warning

--- content/scripts/connect.js
+++ content/scripts/connect.js
@@ -1,3 +1,6 @@
+if (navigator.vendor != "Google Inc.") {
+    alert("Sorry, but Scribe is having a lot of trouble with non-Chrome browsers right now. If something doesn't work, please consider loading this page in Chrome.");
+}
 window.peer = new Peer({ host: 'peerserver.mawteam.com', port: 443, path: '/' });
 
 window.attendees = [ ];
