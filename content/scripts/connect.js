ac041c98b54156fcd945b805dd2afcde0b512dfc
matthewsotoudeh <matthewsot@outlook.com>
2017-02-04 23:40:42 -0800

    changed the peerserver URL

--- content/scripts/connect.js
+++ content/scripts/connect.js
@@ -1,7 +1,7 @@
 if (navigator.vendor != "Google Inc.") {
     alert("Sorry, but Scribe is having a lot of trouble with non-Chrome browsers right now. If something doesn't work, please consider loading this page in Chrome.");
 }
-window.peer = new Peer({ host: 'peerserver.mawteam.com', port: 443, path: '/' });
+window.peer = new Peer({ host: 'mas-peerserver.herokuapp.com', port: 443, path: '/' });
 
 window.attendees = [ ];
 window.clients = [ ];
